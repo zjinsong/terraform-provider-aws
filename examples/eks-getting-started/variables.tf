a21ac9e78ba2d36d5e4772973d66e239cc342249
zjinsong <65064518+zjinsong@users.noreply.github.com>
2020-10-04 04:50:36 +0800

    Update variables.tf

--- examples/eks-getting-started/variables.tf
+++ examples/eks-getting-started/variables.tf
@@ -6,3 +6,14 @@ variable "cluster-name" {
   default = "terraform-eks-demo"
   type    = string
 }
+
+data "aws_ami" "eks_ami" {
+  filter {
+    name   = "name"
+    values = ["amazon-eks-node-1.15-v*"]
+  }
+
+  most_recent = true
+  owners      = ["961992271922"] # Amazon EKS AMI Account ID
+}
+
